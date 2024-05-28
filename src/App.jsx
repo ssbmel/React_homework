0fb0ab04d7f7b089266bba0c0f814ee44721a9db
USERMIC-Q8MSLVD\Administrator <sssb2046@gmail.com>
2024-05-28 10:23:03 +0900

    1st 과제 제출

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,43 @@
+import { useState } from 'react';
 import './App.css'
+import styled from "styled-components";
+
+
+const Container = styled.div`
+  margin: 200px auto;
+  text-align: center;
+`;
+const Count = styled.div`
+  font-size: 200px;
+`;
+const Btn = styled.button`
+  font-size: 50px;
+  margin: 10px;
+  width: 80px;
+`;
+
 
 function App() {
+  const [count, setCount] = useState(0);
 
+  const addCountHandler = () => {
+    setCount(count + 1);
+  }
+
+  const minusCountHandler = () => {
+    setCount(count - 1);
+  }
+
+  const resetCountHandler = () => {
+    setCount(0);
+  }
   return (
-    <div></div>
+    <Container>
+      <Count>{count}</Count>
+      <Btn onClick={addCountHandler}>+</Btn>
+      <Btn onClick={minusCountHandler}>-</Btn>
+      <Btn style={{width: "150px"}} onClick={resetCountHandler}>reset</Btn>
+    </Container>
   );
 }
 
